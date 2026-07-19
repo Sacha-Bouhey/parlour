14e5e91d54d7c94cdb0fdcf82518ce119b50199a
Sacha-Bouhey <lol1c3@proton.me>
2026-07-19 22:43:51 +0200

    added argon2 for password hashing.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Parlour server started!");
+import argon2 from "argon2";
+import e from "express";
+import * as readline from 'node:readline/promises';
+
+async function main() {
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    try{
+        const password = await rl.question('input password');
+        console.log(password);
+        const hash = await argon2.hash(password);
+        console.log(hash);
+
+        const isvalid = await argon2.verify(hash, password);
+
+        if(isvalid){
+            console.log("mot de passe correct");
+    }
+
+
+    } finally {
+        rl.close()
+    }
+
+    
+}
+
+main();
